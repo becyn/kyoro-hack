34cc4fa7ccd59489d2502b5c4b9e9aba7cca8aeb
becyn <yucalcium.c6@gmail.com>
2011-12-29 01:27:38 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KyoroHack::Application.config.secret_token = '4218f38c39a35427a6a17d2f58e5dbce6b02556dd4ec0051db083181298268c55d8287ef469b3088a96d2d3ccd37d055076b5f1e260d2b2f82955e413724c97d'
